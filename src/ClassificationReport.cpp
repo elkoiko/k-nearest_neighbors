1d9d9332f9d8f91b5aab2aacefb44c16272e26bf
elkoiko <tcouacault@gmail.com>
2021-02-16 11:45:12 +0100

    implementing the confusion matrix for classification

--- src/ClassificationReport.cpp
+++ src/ClassificationReport.cpp
@@ -1,11 +1,51 @@
 #include <sstream>
+#include <iomanip>
 #include "ClassificationReport.hpp"
 using namespace std;
 
 ClassificationReport::ClassificationReport(const vector<Similarity>& predictedSimilarities, int k) : _k(k)
 {
     initTagsStatistics(predictedSimilarities);
+    initConfusionMatrix(predictedSimilarities);
     _accuracy = ((double)_ok) / ((double)_nbTags);
+}
+
+void ClassificationReport::initConfusionMatrix(const vector<Similarity>& predictedSimilarities)
+{
+    // Searching for the maximum tag number
+    _confusionMatrixSize = 0;
+    for (const Similarity& similarity: predictedSimilarities)
+    {
+        if (similarity.tag > _confusionMatrixSize) 
+        {
+            _confusionMatrixSize = similarity.tag;
+        }
+    }
+    _confusionMatrixSize += 1; // Add 1 to count the 0 for the size
+
+    // We allocate the _confusionMatrix
+    _confusionMatrix = new int*[_confusionMatrixSize];
+    for(int i = 0; i < _confusionMatrixSize; i++)
+    {
+        _confusionMatrix[i] = new int[_confusionMatrixSize];
+    }
+
+    // Resetting the confusion matrix
+    for (int x = 0; x < _confusionMatrixSize; x++)
+    {
+        for (int y = 0; y < _confusionMatrixSize; y++)
+        {
+            _confusionMatrix[x][y] = 0;
+        }
+    }
+
+    // Filling in the confusion matrix
+    for (const Similarity& similarity: predictedSimilarities)
+    {
+        // Tag is column
+        // Predicted tag is line
+        _confusionMatrix[similarity.predictedTag][similarity.tag] += 1;
+    }
 }
 
 void ClassificationReport::initTagsStatistics(const vector<Similarity>& predictedSimilarities)
@@ -49,5 +89,31 @@ string ClassificationReport::toString()
     strRet << _ok << " tags were well predicted." << endl;
     strRet << getNbWrongTags() << " tags were wrongly predicted." << endl;
     strRet << "For a total of " << _nbTags << " tags, there is an accuracy of " << _accuracy * 100 << "% with k=" << _k << "." << endl;
+    strRet << "*** Confusion Matrix ***" << endl;
+    strRet << "Predicted tags are lines. Real tags are columns." << endl;
+    strRet << " ";
+    for (int i = 0; i < _confusionMatrixSize; i++)
+    {
+        strRet << internal << setw(5) << i;
+    }
+    strRet << endl;
+    for (int x = 0; x < _confusionMatrixSize; x++)
+    {
+        strRet << x << " ";
+        for (int y = 0; y < _confusionMatrixSize; y++)
+        {
+            strRet << internal << setw(4) << _confusionMatrix[x][y] << " ";
+        }
+        strRet << endl;
+    }
     return strRet.str();
+}
+
+ClassificationReport::~ClassificationReport()
+{
+    for (int i = 0; i < _confusionMatrixSize; i++)
+    {
+        delete[] _confusionMatrix[i];
+    }
+    delete[] _confusionMatrix;
 }
--- src/ClassificationReport.hpp
+++ src/ClassificationReport.hpp
@@ -13,11 +13,16 @@ public:
     int getNbWrongTags() const;
     int getNbTotalTags() const;
     double getAccuracy() const;
+
+    ~ClassificationReport();
 private:
+    int **_confusionMatrix;
+    int _confusionMatrixSize;
     double _accuracy;
     int _nbTags;
     int _ok;
     int _k;
 
     void initTagsStatistics(const std::vector<Similarity>& predictedSimilarities);
+    void initConfusionMatrix(const std::vector<Similarity>& predictedSimilarities);
 };
